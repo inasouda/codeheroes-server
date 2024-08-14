cc6e460834918b3b43b6cd3b76a68672279787bc
Inas Ouda <inasouda@Inass-Laptop.local>
2024-08-13 23:50:36 -0600

    add request free lesson

--- controllers/free-lesson.controller.js
+++ controllers/free-lesson.controller.js
@@ -2,7 +2,7 @@ const RequestFreeLesson = require('../models/freeLesson.model');
 const Student = require ('../models/student.model')
 
 const requestFreeLesson = async (req,res) =>{
-    console.log('hello')
+    console.log('request free lesson...')
     const freeLesson = await RequestFreeLesson.create(req.body)
     res.send(freeLesson);
 }
