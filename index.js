5b218201081ba2ab324e5b24efb6657e400429ac
Inas Ouda <inasouda@Inass-Laptop.local>
2024-08-14 23:37:02 -0600

    update origions

--- index.js
+++ index.js
@@ -8,13 +8,25 @@ const cors = require('cors');
 const app = express();
 
 // Configure CORS
-const corsOptions = {
-    origin: 'https://codeheroes.vercel.app', // Replace with your frontend URL
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'], // Allow the methods you need
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
-  };
+// const corsOptions = {
+//     origin: 'https://codeheroes.vercel.app', // Replace with your frontend URL
+//     methods: ['GET', 'POST', 'PATCH', 'DELETE'], // Allow the methods you need
+//     allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+//   };
+  const allowedOrigins = [
+    'https://www.thecodeheroes.com',
+    'https://codeheroes.vercel.app',
+  ];
   
-  app.use(cors(corsOptions));
+  app.use(cors({
+    origin: function(origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }
+  }));
 
 app.use(express.json());  
 app.use("/api/v1/students",studentsRouter);
